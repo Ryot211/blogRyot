568e3e2dc9ec085034477d7f37be1775a29974fa
Ryot211 <ryot211@gmail.com>
2025-09-19 23:53:20 -0500

    estilos y modelos

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Articulo,Comentario
 
-# Register your models here.
+admin.site.register(Articulo)
+admin.site.register(Comentario)
--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+#Articulo
+class Articulo(models.Model):
+    id_art =models.AutoField(primary_key=True)
+    titulo_art = models.CharField(max_length=100)
+    contenido_art=models.CharField(max_length=350)
+    autor_art=models.ForeignKey(User, on_delete=models.CASCADE)
+    creacion_art=models.DateField(auto_now_add=True)
+    actualizacion_art=models.DateField(auto_now=True)
+#Comentario
+class Comentario(models.Model):
+    id_comen= models.AutoField(primary_key=True)
+    articulo=models.ForeignKey(Articulo, null=True,blank=True,on_delete=models.PROTECT)
+    autor_comen=models.ForeignKey(User,on_delete=models.CASCADE)
+    contenido_comen= models.CharField(max_length=250)
+    creacion_comen=models.DateField(auto_now_add=True)
