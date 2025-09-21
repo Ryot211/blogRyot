34b99c04150ca883052871d2f1d0947e819c7507
Ryot211 <ryot211@gmail.com>
2025-09-21 00:29:19 -0500

    insert

--- Blog/urls.py
+++ Blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views  # Asegúrate de que estás importando las vistas correctas
 
 urlpatterns = [
-   path('',views.listadoArticulo)
+   path('',views.listadoArticulo),
+   path('CrearArticulo/',views.CrearArticulo),
+   
 ]
--- Blog/views.py
+++ Blog/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Articulo
 
 #Articulo
 def listadoArticulo (request):
     articulosBdd= Articulo.objects.all()
-    return render (request, "ListarBlog.html",{'articuloss':articulosBdd})
+    
+    return render (request, "Muro.html",{'articuloss':articulosBdd})
 
+def CrearArticulo(request):
+    if request.method == "POST":
+        titulo_art = request.POST["titulo_art"]
+        contenido_art = request.POST["contenido_art"]
+        articulo = Articulo(
+            titulo_art=titulo_art,
+            contenido_art=contenido_art,
+            autor_art=request.user
+        )
+        articulo.save()
+        messages.success(request,"Post Creado")
+        return redirect("/")  
 # Create your views here.
